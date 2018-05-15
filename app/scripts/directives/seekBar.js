ef22f5c9d4fd86ec74f49230bac64716e0ae5af6
Kasper Pickett <kasperpickett@gmail.com>
2018-05-15 00:33:00 -0500

    added scope.thumbStyle function

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -13,24 +13,14 @@
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
-      scope: {
-        onChange: '&'
-      },
+      scope: { },
       link: function(scope, element, attributes) {
         scope.value = 0;
         scope.max = 100;
 
         var seekBar = $(element);
 
-        attributes.$observe('value', function(newValue) {
-          scope.value = newValue;
-        });
-
-        attributes.$observe('max', function(newValue) {
-          scope.max = newValue;
-        });
-
-        var percentString = function() {
+        var percentString = function () {
           var value = scope.value;
           var max = scope.max;
           var percent = value / max * 100;
@@ -48,7 +38,6 @@
         scope.onClickSeekBar = function(event) {
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
-          notifyOnChange(scope.value);
         };
 
         scope.trackThumb = function() {
@@ -56,7 +45,6 @@
             var percent = calculatePercent(seekBar, event);
             scope.$apply(function() {
               scope.value = percent * scope.max;
-              notifyOnChange(scope.value);
             });
           });
 
@@ -65,17 +53,11 @@
             $document.unbind('mouseup.thumb');
           });
         };
-
-        var notifyOnChange = function(newValue) {
-          if (typeof scope.onChange === 'function') {
-            scope.onChange({value: newValue});
-          }
-        };
       }
     };
   }
 
-    angular
-        .module('blocJams')
-        .directive('seekBar', ['$document', seekBar]);
+  angular
+    .module('blocJams')
+    .directive('seekBar', ['$document', seekBar]);
 })();
